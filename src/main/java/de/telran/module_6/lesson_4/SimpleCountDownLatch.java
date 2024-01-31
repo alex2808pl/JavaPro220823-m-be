3550f0c88d1bda1b6460ec6caa87df33bca3defc
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-01-31 12:02:34 +0200

    module_6 lesson_4 concurrent - 1

---
+++ src/main/java/de/telran/module_6/lesson_4/SimpleCountDownLatch.java
@@ -0,0 +1,55 @@
+package de.telran.module_6.lesson_4;
+
+
+import java.util.concurrent.CountDownLatch;
+
+// пример применения CountDownLatch в процессе отправления людей в отпуск, у нас 8 человек, которые
+// желают отправиться в отпуск. В аэропорту они выполняют обычные действия, такие как регистрация,
+// сдача багажа. Делают они это по отдельности, а после этого уже группой садятся в самолет и вылетают
+public class SimpleCountDownLatch {
+    public static void main(String[] args) throws Exception {
+
+        CountDownLatch countDownLatch = new CountDownLatch(8);
+
+        // список людей, кто желает полететь на отдых
+        String[] personNames = new String[]{"Roman", "Alena", "Kira", "Anna", "Leo", "Bob", "Vladislav", "Inna"};
+
+        for (int i = 0; i < personNames.length; i++) {
+            new Thread(new PersonInAirport(personNames[i], countDownLatch)).start();
+            Thread.sleep(700);
+        }
+
+    }
+}
+
+class PersonInAirport implements Runnable {
+    private String personName;
+    private CountDownLatch countDownLatch;
+
+    public PersonInAirport(String personName, CountDownLatch countDownLatch) {
+        this.personName = personName;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            // список дел, которые буду выполнены в рамках каждого потока (человека) самостоятельно
+            System.out.println("-> " + personName + " приехал в аэропорт.");
+            System.out.println("[!] " + personName + " сдал багаж");
+            System.out.println("[!] " + personName + " успел перекусить");
+
+            countDownLatch.countDown(); //-1
+
+            System.out.println();
+            countDownLatch.await(); // когда count==0 все выходят из состояния ожидания
+
+            // финальное - выполнится то, что находится после await() метода
+            System.out.println(personName + " группой вылетел на отдых");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
